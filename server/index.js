eac74ea5b348a08942355e2e92b9395da1161ae8
SNijanthan <nijanthan378@gmail.com>
2026-03-24 19:57:50 +0530

    feat: cors config

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
 const connectToDB = require("./config/database.js");
 const authRouter = require("./routers/authRouter.js");
 const doctorRouter = require("./routers/doctorRouter.js");
@@ -10,6 +12,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors({ origin: "http://localhost:5000", credentials: true }));
 
 const port = process.env.PORT || 5000;
 
