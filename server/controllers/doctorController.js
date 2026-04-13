b3700a59aec2d65aecf60883cd158f8d72632d9f
SNijanthan <nijanthan378@gmail.com>
2026-04-13 15:39:12 +0530

    fix: Get all doctors controller modified for better response

--- server/controllers/doctorController.js
+++ server/controllers/doctorController.js
@@ -2,25 +2,7 @@ const Doctor = require("../models/doctor.js");
 
 const getDoctors = async (req, res) => {
   try {
-    // * 1. Get query params
-    let { page = 1, limit = 10 } = req.query;
-
-    // * 2. Convert to numbers
-    page = parseInt(page);
-    limit = parseInt(limit);
-
-    // * 3. Prevent abuse
-    limit = Math.min(limit, 50);
-
-    // * 4. Calculate skip
-    const skip = (page - 1) * limit;
-
-    const doctors = await Doctor.find()
-      .select("-password")
-      .sort({ createdAt: -1 })
-      .skip(skip)
-      .limit(limit)
-      .lean();
+    const doctors = await Doctor.find().select("-password");
 
     const total = await Doctor.countDocuments();
 
@@ -28,12 +10,6 @@ const getDoctors = async (req, res) => {
       status: true,
       message: "Data fetched successfully",
       data: doctors,
-      pagination: {
-        page,
-        limit,
-        total,
-        totalPages: Math.ceil(total / limit),
-      },
     });
   } catch (error) {
     res.status(500).json({ status: false, message: error.message });
