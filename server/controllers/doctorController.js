60dc3662c78fb11fbbaddbc465e4e79b478d1d69
SNijanthan <nijanthan378@gmail.com>
2026-04-14 00:14:11 +0530

    feat(api): add doctor filtering by specialty and improve validation logic

--- server/controllers/doctorController.js
+++ server/controllers/doctorController.js
@@ -36,4 +36,49 @@ const getDoctorById = async (req, res) => {
   }
 };
 
-module.exports = { getDoctors, getDoctorById };
+const getDoctorBySpecialty = async (req, res) => {
+  try {
+    const specialties = [
+      "general-physician",
+      "dermatology",
+      "psychiatry",
+      "pediatrics",
+      "gastroenterology",
+      "cardiology",
+      "orthopedics",
+      "neurology",
+      "gynecology",
+      "ent",
+    ];
+    const { specialty } = req.query;
+
+    if (!specialties.includes(specialty)) {
+      return res
+        .status(400)
+        .json({ statue: false, message: "Invalid specialty" });
+    }
+    const doctors = await Doctor.find({ specialties: specialty }).select(
+      "-password",
+    );
+
+    if (doctors.length === 0) {
+      res.status(200).json({
+        status: true,
+        message: "No data to retrieve",
+      });
+    }
+
+    return res.status(200).json({
+      status: true,
+      message: `Doctors with ${specialty} retrieved successfully`,
+      doctors,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = { getDoctors, getDoctorById, getDoctorBySpecialty };
--- server/routers/doctorRoutes.js
+++ server/routers/doctorRoutes.js
@@ -5,11 +5,13 @@ const doctorRouters = express.Router();
 const {
   getDoctors,
   getDoctorById,
+  getDoctorBySpecialty,
 } = require("../controllers/doctorController.js");
 
 const tokenAuth = require("../middleware/authMiddleware.js");
 
 doctorRouters.get("/doctors", tokenAuth, getDoctors);
 doctorRouters.get("/doctor/:id", tokenAuth, getDoctorById);
+doctorRouters.get("/doctors/specialty", tokenAuth, getDoctorBySpecialty);
 
 module.exports = doctorRouters;
