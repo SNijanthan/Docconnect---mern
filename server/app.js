efde8cc35cd40c610e07dc0f0df881d4f35a55f2
SNijanthan <nijanthan378@gmail.com>
2026-04-20 12:28:39 +0530

    fix: CORS issue fixed

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", process.env.CLIENT_URL],
+    origin: [process.env.CLIENT_URL],
     credentials: true,
   }),
 );
